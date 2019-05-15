6d9b8d0faf095fd9c103bae81a6dc6aea94654e3
EC2 Default User <ec2-user@ip-172-31-38-244.us-east-2.compute.internal>
2019-05-15 20:42:04 +0000

    Database access and updates

--- indexActions.js
+++ indexActions.js
@@ -1,5 +1,9 @@
+var scoreCount;
+
+setInterval(getCurrentScores, 500);
+
 function setHouseText(text){
-    document.getElementById("houseText").setAttribute("value", text);
+    document.getElementById("houseText").value = text;
 }
 
 function success(){
@@ -8,4 +12,52 @@ function success(){
 
 function error(){
     console.log("Could not complete entry");
+}
+
+function updateDatabase(){
+    firebase.database().ref().child('scores').set(scoreCount += 1);
+}
+
+function saveToFirebase() {
+    var selectedHouse = document.getElementById("houseText").value;
+    var addedValue = parseInt(document.getElementById("points").value);
+    
+    if(!isNaN(addedValue) && selectedHouse != ""){
+        var scoreToSet;
+        firebase.database().ref(selectedHouse).once('value').then(function(dataSnapshot) {
+            scoreToSet = dataSnapshot.val();
+            firebase.database().ref(selectedHouse).set(scoreToSet + addedValue)
+            .then(function(snapshot) {
+                success(); // some success method
+            }, function(error) {
+                console.log('error' + error);
+                error(); // some error method
+            });
+        });
+    }
+    
+    getCurrentScores();
+}
+
+function getCurrentScores(){
+    var house1;
+    var house2;
+    var house3;
+    var house4;
+    firebase.database().ref('House 1').once('value').then(function(dataSnapshot) {
+        house1 = dataSnapshot.val();
+        document.getElementById("h1Score").innerHTML = house1;
+    });
+    firebase.database().ref('House 2').once('value').then(function(dataSnapshot) {
+        house2 = dataSnapshot.val();
+        document.getElementById("h2Score").innerHTML = house2;
+    });
+    firebase.database().ref('House 3').once('value').then(function(dataSnapshot) {
+        house3 = dataSnapshot.val();
+        document.getElementById("h3Score").innerHTML = house3;
+    });
+    firebase.database().ref('House 4').once('value').then(function(dataSnapshot) {
+        house4 = dataSnapshot.val();
+        document.getElementById("h4Score").innerHTML = house4;
+    });
 }
