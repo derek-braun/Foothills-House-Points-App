aea4ded984274713257be5379b3390e9a91cc9c6
EC2 Default User <ec2-user@ip-172-31-38-244.us-east-2.compute.internal>
2019-05-17 00:10:31 +0000

    Add passcode and alert

--- indexActions.js
+++ indexActions.js
@@ -19,32 +19,45 @@ function error(){
 async function saveToFirebase() {
     var selectedHouse = document.getElementById("houseText").value;
     var addedValue = parseInt(document.getElementById("points").value);
+    var enteredPasscode = parseInt(document.getElementById("passcode").value);
     
-    // Only perform action if the required fields are not empty
-    if(selectedHouse == "Griffin" || selectedHouse == "Dragon" || selectedHouse == "Phoenix" || selectedHouse == "Titan"){
-        if(!isNaN(addedValue) && selectedHouse != ""){
-            var scoreToSet;
-            firebase.database().ref(selectedHouse).once('value').then(function(dataSnapshot) {
-                scoreToSet = dataSnapshot.val();
-                firebase.database().ref(selectedHouse).set(scoreToSet + addedValue)
-                .then(function(snapshot) {
-                    success(); // some success method
-                }, function(error) {
-                    console.log('error' + error);
-                    error(); // some error method
-                });
-            });
+    firebase.database().ref("passcode").once('value').then(function(dataSnapshot) {
+        if(enteredPasscode == dataSnapshot.val()){
+            // Only perform action if the required fields are not empty
+            if(selectedHouse == "Griffin" || selectedHouse == "Dragon" || selectedHouse == "Phoenix" || selectedHouse == "Titan"){
+                if(!isNaN(addedValue) && selectedHouse != ""){
+                    var scoreToSet;
+                    firebase.database().ref(selectedHouse).once('value').then(function(dataSnapshot) {
+                        scoreToSet = dataSnapshot.val();
+                        firebase.database().ref(selectedHouse).set(scoreToSet + addedValue)
+                        .then(function(snapshot) {
+                            success(); // some success method
+                        }, function(error) {
+                            console.log('error' + error);
+                            error(); // some error method
+                        });
+                    });
+                }
+            }
         }
-    }
+        else {
+            alert("Incorrect Passcode");
+        }
+    })
     
     // Refresh scores after update has to wait for short time to update database
-    await sleep(500);
+    await sleep(1000);
     getCurrentScores();
 }
 
 // Sleep process for specified time
 function sleep(msTime){
     return new Promise(resolve => setTimeout(resolve, msTime));
+}
+
+function clearInputs(){
+    document.getElementById("houseText").value = "";
+    document.getElementById("points").value = "";
 }
 
 // Retrieve and display current scores from database
@@ -56,21 +69,19 @@ function getCurrentScores(){
     firebase.database().ref('Dragon').once('value').then(function(dataSnapshot) {
         house1 = dataSnapshot.val();
         document.getElementById("h1Score").innerHTML = house1;
-        document.getElementById("h1Score").style.fontWeight = "900";
     });
     firebase.database().ref('Griffin').once('value').then(function(dataSnapshot) {
         house2 = dataSnapshot.val();
         document.getElementById("h2Score").innerHTML = house2;
-        document.getElementById("h2Score").style.fontWeight = "900";
     });
     firebase.database().ref('Phoenix').once('value').then(function(dataSnapshot) {
         house3 = dataSnapshot.val();
         document.getElementById("h3Score").innerHTML = house3;
-        document.getElementById("h3Score").style.fontWeight = "900";
     });
     firebase.database().ref('Titan').once('value').then(function(dataSnapshot) {
         house4 = dataSnapshot.val();
         document.getElementById("h4Score").innerHTML = house4;
-        document.getElementById("h4Score").style.fontWeight = "900";
     });
+    
+    clearInputs();
 }
